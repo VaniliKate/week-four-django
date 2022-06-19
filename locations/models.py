0422359db221559146e3a7d67962ce18ae7574a9
Vanili <katevanili98@gmail.com>
2022-06-19 21:15:24 +0300

    Installed dependencies

--- locations/models.py
+++ locations/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from pyuploadcare.dj.models import ImageField
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # Create your models here.
