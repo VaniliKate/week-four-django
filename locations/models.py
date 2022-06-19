0241b8e7be79057b5916abd35cba192b4ac3e9d0
Vanili <katevanili98@gmail.com>
2022-06-19 21:22:44 +0300

    Created the profile model

--- locations/models.py
+++ locations/models.py
@@ -7,11 +7,14 @@ from django.db.models.signals import post_save
 
 # Create your models here.
 
+
 class NeighbourHood(models.Model):
     name = models.CharField(max_length=50)
     location = models.CharField(max_length=60)
-    admin = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name='hood')
-    hood_logo =  CloudinaryField('profile_photos/', default='profile_photos/user')
+    admin = models.ForeignKey(
+        "Profile", on_delete=models.CASCADE, related_name='hood')
+    hood_logo = CloudinaryField(
+        'profile_photos/', default='profile_photos/user')
     description = models.TextField()
     health_info = models.IntegerField(null=True, blank=True)
     health_officer = models.CharField(max_length=60, null=True, blank=True)
@@ -30,3 +33,15 @@ class NeighbourHood(models.Model):
     @classmethod
     def find_neighborhood(cls, neighborhood_id):
         return cls.objects.filter(id=neighborhood_id)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name='profile')
+    name = models.CharField(max_length=80, blank=True)
+    bio = models.TextField(max_length=254, blank=True)
+    profile_picture = CloudinaryField(
+        'profile_photos/', default='profile_photos/user')
+    location = models.CharField(max_length=50, blank=True, null=True)
+    neighbourhood = models.ForeignKey(
+        NeighbourHood, on_delete=models.SET_NULL, null=True, related_name='members', blank=True)
