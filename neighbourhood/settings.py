0a837f1bbc8519b44618a6bf15a343fe42754777
Vanili <katevanili98@gmail.com>
2022-06-19 22:06:16 +0300

    New file changes

--- neighbourhood/settings.py
+++ neighbourhood/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+export DJANGO_SETTINGS_MODULE=mysites.settings
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
