f221ebfe953499c34e36e366e66127e54ae031e1
Vanili <katevanili98@gmail.com>
2022-06-19 21:16:28 +0300

    Created the NeighbourHood model

--- locations/models.py
+++ locations/models.py
@@ -6,3 +6,14 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 
 # Create your models here.
+
+class NeighbourHood(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=60)
+    admin = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name='hood')
+    hood_logo =  CloudinaryField('profile_photos/', default='profile_photos/user')
+    description = models.TextField()
+    health_info = models.IntegerField(null=True, blank=True)
+    health_officer = models.CharField(max_length=60, null=True, blank=True)
+    police_info = models.IntegerField(null=True, blank=True)
+    police_officer = models.CharField(max_length=60, null=True, blank=True)
