ed40a126f178afa39349654e93cde752f53ce5bc
Vanili <katevanili98@gmail.com>
2022-06-19 21:17:24 +0300

    Neighbourhood model methods

--- locations/models.py
+++ locations/models.py
@@ -17,3 +17,16 @@ class NeighbourHood(models.Model):
     health_officer = models.CharField(max_length=60, null=True, blank=True)
     police_info = models.IntegerField(null=True, blank=True)
     police_officer = models.CharField(max_length=60, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.name} hood'
+
+    def create_neighborhood(self):
+        self.save()
+
+    def delete_neighborhood(self):
+        self.delete()
+
+    @classmethod
+    def find_neighborhood(cls, neighborhood_id):
+        return cls.objects.filter(id=neighborhood_id)
